b85aa6481637f54e47f60b3f7396f36f52d57abe
CWalsh411 <Connwalsh2244@gmail.com>
2018-10-24 13:59:52 -0400

    update assignment 4

--- assignment4.cpp
+++ assignment4.cpp
@@ -9,41 +9,43 @@ int backtrack(int*, int);
 void print(int*, int);
 
 int main(){
-  int b[8] = {8}, c = -1;
+  int b[8] = {8}, c = 0;
+  b[c] = 0;
   newcol(b, c);
   return 0;
 }
 void newcol(int* b, int c){
   c++;
-  cout << c << endl;
   if(c == 8)
     print(b, c);
   b[c] = -1;
   newrow(b, c);
 }
 void newrow(int* b, int c){
-  cout << "here" << endl;
   b[c]++;
   if(b[c] == 8)
     backtrack(b, c);
   else{
-    for(int i = 0; i < (c + 1); i++){
-        cout << i << endl;
-        if(b[c] == b[i] || b[c - i] == (b[c] - i) || b[c - i] == (b[c] + i))
+    for(int i = 0; i < c; i++){
+        if(b[c] == b[i] || (c - i) == abs(b[c] - b[i]))
           newrow(b, c);
-        newcol(b, c);
     }
+  newcol(b, c);
   }
 }
 
 int backtrack(int* b, int c){
   c--;
-  cout << "here0" << endl;
   if(c == -1)
     return 0;
   newrow(b,c);
 }
 void print(int* b, int c){
+
+  for(int i = 0; i < 8; i++)
+    cout << b[i];
+  cout << endl;
+/*
   cout << "\n";
   for(int i = 0; i < 8; i++){
     cout << "\n";
@@ -54,5 +56,6 @@ void print(int* b, int c){
         cout << 0;
       }
     }
+*/
   backtrack(b, c);
 }
